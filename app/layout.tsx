1881b22e619eaed906f94ae964489f520c49a0c0
syeonnn <qkrtj418@gmail.com>
2024-07-30 15:34:26 +0900

    next-dashboard 연습

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+
+import { inter } from './ui/fonts';
+
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* # 2-1-2. body 태그에 inter 폰트 적용 */}
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// 외부에서 import해 사용할 수 있도록
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight: "400",subsets: ['latin']});
